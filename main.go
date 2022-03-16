8a3a474ce8d4c05df5ef0dc2da12944d73edbd53
mnanri <nanristeps@icloud.com>
2022-03-16 22:40:30 +0900

    make transpose

--- main.go
+++ main.go
@@ -1,17 +1,20 @@
 package main
 
 import (
+	"conversion/pkg/cipher"
 	"fmt"
 )
 
 func main() {
 	//動作テスト用
 	//var v []string = cipher.Ceaser([]string{"abc", "def", "ghi", "jkl"}, 4)
+	//fmt.Println('あ')
 
 	//var v []string = []string{"abc", "lmn", "xyz", "ABC"}
 	//for i := 0; i < 26; i++ {
 	//	fmt.Println(cipher.Ceaser(v, i))
 	//}
 
-	fmt.Println('あ')
+	v := "abcdefghijklmnop"
+	fmt.Println(cipher.Transpose(v, 5))
 }
---
+++ pkg/cipher/transpose.go
@@ -0,0 +1,51 @@
+package cipher
+
+import (
+	"conversion/pkg"
+	"math/rand"
+	"strings"
+)
+
+func Transpose(s string, n int) string {
+	t := strings.Split(s, "")
+	seg := ""
+	var ss []string = []string{}
+	for i := 1; i <= len(s); i++ {
+		seg = seg + t[i-1]
+		if i%5 == 0 {
+			ss = append(ss, seg)
+			seg = ""
+		}
+	}
+	if seg != "" {
+		ss = append(ss, seg)
+	}
+
+	var order []int = []int{}
+	for i := 0; i < n; i++ {
+		order = append(order, i)
+	}
+	for i := n - 1; i > 0; i-- {
+		j := rand.Intn(i + 1)
+		order[i], order[j] = order[j], order[i]
+	}
+
+	var ress []string = pkg.Map(ss, func(u string) string {
+		tmp := ""
+		if len(u) == n {
+			var a []string = strings.Split(u, "")
+			for i := 0; i < n; i++ {
+				tmp = tmp + a[order[i]]
+			}
+		} else {
+			tmp = u
+		}
+		return tmp
+	})
+
+	res := ""
+	for i := 0; i < len(ress); i++ {
+		res = res + ress[i]
+	}
+	return res
+}
