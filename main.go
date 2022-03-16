98c693d8fa081f99508ae05cecab7e8c6f893066
mnanri <nanristeps@icloud.com>
2022-03-16 19:07:37 +0900

    makefile

--- main.go
+++ main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"conversion/pkg/cipher"
 	"fmt"
 )
 
@@ -9,8 +8,10 @@ func main() {
 	//動作テスト用
 	//var v []string = cipher.Ceaser([]string{"abc", "def", "ghi", "jkl"}, 4)
 
-	var v []string = []string{"abc", "lmn", "xyz", "ABC"}
-	for i := 0; i < 26; i++ {
-		fmt.Println(cipher.Ceaser(v, i))
-	}
+	//var v []string = []string{"abc", "lmn", "xyz", "ABC"}
+	//for i := 0; i < 26; i++ {
+	//	fmt.Println(cipher.Ceaser(v, i))
+	//}
+
+	fmt.Println('あ')
 }
